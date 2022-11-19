5d82ab945b95f10e6293f7a5bdb7bf6a866211ff
coder-omer <60143421+coder-omer@users.noreply.github.com>
2022-11-19 13:47:31 +0300

    E-Commerce Data and Customer Retention Analysis

---
+++ RDB&SQL - E Commerce Data Analysis Project/E-Commerce Project DraftFile.sql
@@ -0,0 +1,174 @@
+
+
+--DAwSQL Session -8 
+
+--E-Commerce Project Solution
+
+
+
+--1. Join all the tables and create a new table called combined_table. (market_fact, cust_dimen, orders_dimen, prod_dimen, shipping_dimen)
+
+
+
+
+
+--///////////////////////
+
+
+--2. Find the top 3 customers who have the maximum count of orders.
+
+
+
+
+
+--/////////////////////////////////
+
+
+
+--3.Create a new column at combined_table as DaysTakenForDelivery that contains the date difference of Order_Date and Ship_Date.
+--Use "ALTER TABLE", "UPDATE" etc.
+
+
+
+
+--////////////////////////////////////
+
+
+--4. Find the customer whose order took the maximum time to get delivered.
+--Use "MAX" or "TOP"
+
+
+
+
+--////////////////////////////////
+
+
+
+--5. Count the total number of unique customers in January and how many of them came back every month over the entire year in 2011
+--You can use date functions and subqueries
+
+
+
+
+
+--////////////////////////////////////////////
+
+
+--6. write a query to return for each user acording to the time elapsed between the first purchasing and the third purchasing, 
+--in ascending order by Customer ID
+--Use "MIN" with Window Functions
+
+
+
+
+
+--//////////////////////////////////////
+
+--7. Write a query that returns customers who purchased both product 11 and product 14, 
+--as well as the ratio of these products to the total number of products purchased by all customers.
+--Use CASE Expression, CTE, CAST and/or Aggregate Functions
+
+
+
+
+--/////////////////
+
+
+
+--CUSTOMER SEGMENTATION
+
+
+
+--1. Create a view that keeps visit logs of customers on a monthly basis. (For each log, three field is kept: Cust_id, Year, Month)
+--Use such date functions. Don't forget to call up columns you might need later.
+
+
+
+--//////////////////////////////////
+
+
+
+  --2.Create a “view” that keeps the number of monthly visits by users. (Show separately all months from the beginning  business)
+--Don't forget to call up columns you might need later.
+
+
+
+
+
+--//////////////////////////////////
+
+
+--3. For each visit of customers, create the next month of the visit as a separate column.
+--You can order the months using "DENSE_RANK" function.
+--then create a new column for each month showing the next month using the order you have made above. (use "LEAD" function.)
+--Don't forget to call up columns you might need later.
+
+
+
+--/////////////////////////////////
+
+
+
+--4. Calculate monthly time gap between two consecutive visits by each customer.
+--Don't forget to call up columns you might need later.
+
+
+
+
+
+
+
+--///////////////////////////////////
+
+
+--5.Categorise customers using average time gaps. Choose the most fitted labeling model for you.
+--For example: 
+--Labeled as “churn” if the customer hasn't made another purchase for the months since they made their first purchase.
+--Labeled as “regular” if the customer has made a purchase every month.
+--Etc.
+	
+
+
+
+
+
+
+
+--/////////////////////////////////////
+
+
+
+
+--MONTH-WISE RETENTÝON RATE
+
+
+--Find month-by-month customer retention rate  since the start of the business.
+
+
+--1. Find the number of customers retained month-wise. (You can use time gaps)
+--Use Time Gaps
+
+
+
+
+
+--//////////////////////
+
+
+--2. Calculate the month-wise retention rate.
+
+--Basic formula: o	Month-Wise Retention Rate = 1.0 * Number of Customers Retained in The Current Month / Total Number of Customers in the Current Month
+
+--It is easier to divide the operations into parts rather than in a single ad-hoc query. It is recommended to use View. 
+--You can also use CTE or Subquery if you want.
+
+--You should pay attention to the join type and join columns between your views or tables.
+
+
+
+
+
+
+
+---///////////////////////////////////
+--Good luck!
